e7e96757da89dce79a166321a49a9b3ce4c71a3e
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2021-06-10 03:05:24 +0200

    Updates

---
+++ precache-manifest.574e870ae0c4859d6045ec082562bdb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499bd15082febee00d49f2fbe82b17b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affb0192899a78887b3b",
+    "url": "/static/css/2.c7a63f68.chunk.css"
+  },
+  {
+    "revision": "caa2231f502f2474311e",
+    "url": "/static/css/main.0415814b.chunk.css"
+  },
+  {
+    "revision": "affb0192899a78887b3b",
+    "url": "/static/js/2.d8595395.chunk.js"
+  },
+  {
+    "revision": "caa2231f502f2474311e",
+    "url": "/static/js/main.50407cbf.chunk.js"
+  },
+  {
+    "revision": "43bf7ce7101433e21cd2",
+    "url": "/static/js/runtime-main.f151a2f3.js"
+  }
+]);
