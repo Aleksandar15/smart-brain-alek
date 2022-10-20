2fce720c965d25f7290af8d1d1c30f246871443c
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-10-21 00:16:06 +0200

    Switching away from HEROKU

--- src/components/User/Scripts/onLogReg.js
+++ src/components/User/Scripts/onLogReg.js
@@ -1,58 +1,72 @@
 //1. fetch and return user based on if and statment (register or login?)
 //2. check user is legitimate then loadUser
 
-
-const onLogReg = (loadUser, onRouteChange, type, email, password,name, wrongDetails, guest) => {
-    console.log(loadUser, onRouteChange,type, email, password, name, wrongDetails, guest);
-    if (type === 'register'){
-        console.log('register')
-        fetch('https://warm-temple-63763.herokuapp.com/register', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            email: email,
-            password: password,
-            name: name
-        })
-        }) 
-        .then(response => response.json())
-        .then(user => {
-            if(user.id){
-                //loadUser
-                loadUser(user);
-                onRouteChange('home')
-            } else {
-                wrongDetails()
-            }
-        })
-    } else {
-        console.log('log in')
-        if (guest === 'guest') {
-            email = "guest@gmail.com";
-            password = "password";
+const onLogReg = (
+  loadUser,
+  onRouteChange,
+  type,
+  email,
+  password,
+  name,
+  wrongDetails,
+  guest
+) => {
+  // console.log(
+  //   loadUser,
+  //   onRouteChange,
+  //   type,
+  //   email,
+  //   password,
+  //   name,
+  //   wrongDetails,
+  //   guest
+  // );
+  if (type === "register") {
+    console.log("register");
+    fetch("https://smart-brain-alek-api.up.railway.app/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        name: name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          //loadUser
+          loadUser(user);
+          onRouteChange("home");
+        } else {
+          wrongDetails();
         }
-        fetch('https://warm-temple-63763.herokuapp.com/signin', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-        })
-        .then(response =>  response.json() )
-        .then(user => {
-            if(user.id){
-                //loadUser
-                loadUser(user);
-                onRouteChange('home')
-            } else {
-                wrongDetails()
-            }
-        })
-    
-        
+      });
+  } else {
+    console.log("log in");
+    if (guest === "guest") {
+      email = "guest@gmail.com";
+      password = "password";
     }
-}
+    fetch("https://smart-brain-alek-api.up.railway.app/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          //loadUser
+          loadUser(user);
+          onRouteChange("home");
+        } else {
+          wrongDetails();
+        }
+      });
+  }
+};
 
 export default onLogReg;
-
